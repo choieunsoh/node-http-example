103b0685c0a42cb187d8356578f7bf843a5a2d05
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-01-24 22:02:25 +0700

    add html page to serve by node

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const hostname = "localhost";
 const port = 3333;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log(`Request for ${req.url} by method ${req.method}`);
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.end("<h1>Hello, World</h1>");
+  if (req.method === "GET") {
+    const fileUrl = req.url === "/" ? "/index.html" : req.url;
+    const filePath = path.resolve(`./public${fileUrl}`);
+    const fileExt = path.extname(filePath);
+    if (fileExt === ".html") {
+      fs.access(filePath, (exists) => {
+        if (exists === false) {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html; charset=utf-8");
+          res.end(`<h1>404 Page Not Found</h1><p>${fileUrl}</p>`);
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/html; charset=utf-8");
+          fs.createReadStream(filePath).pipe(res);
+        }
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.end(`<h1>404 Not An HTML FIle</h1><p>${fileUrl}</p>`);
+    }
+  } else {
+    res.statusCode = 405;
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(`<h1>405 Method Not Supported</h1>`);
+  }
 });
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server listening on port http://${hostname}:${port}`);
 });

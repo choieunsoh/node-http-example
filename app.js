2d54b8e38ff0ca19509b9916cae4fd99fbeb139c
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-01-25 08:07:15 +0700

    add morgan log into file

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const path = require("path");
+const fs = require("fs");
 
 const hostname = "localhost";
 const port = 3000;
 
+const accessLog = fs.createWriteStream(
+  path.join(__dirname, "log", "access.log"),
+  { flags: "a" }
+);
+
 const app = express();
-app.use(morgan("dev"));
+app.use(morgan("combined", { stream: accessLog }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
